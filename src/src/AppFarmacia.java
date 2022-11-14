4d716f2ba25e458a4ec092933600a927facd7403
p4tria <vrmdim45@gmail.com>
2022-11-14 20:13:11 +0100

    hello world

---
+++ src/src/AppFarmacia.java
@@ -0,0 +1,12 @@
+package src;
+
+public class AppFarmacia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+		
+	}
+
+}
